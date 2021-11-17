6d9440fba66a2fb917968dc6d319bed72153a6c3
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2021-11-17 17:43:00 +0000

    sandbox gauss

--- gaudi/gauss/runner.sh
+++ gaudi/gauss/runner.sh
@@ -1,6 +1,9 @@
 source /cvmfs/lhcb.cern.ch/lib/LbEnv
 lb-set-platform x86_64-slc6-gcc49-opt
 
+mkdir -p ${SANDBOX}
+cd ${SANDBOX}
+
 lb-run -c best Gauss/${GAUSS_VERSION} gaudirun.py \
 ${JOB_FILE} \
 ${APPCONFIGOPTS}/Gauss/Beam6500GeV-md100-${YEAR}-nu1.6.py \
@@ -10,4 +13,6 @@ ${APPCONFIGOPTS}/Gauss/RICHRandomHits.py \
 ${DECFILESROOT}/options/${EVENT_TYPE}.py \
 /cvmfs/lhcb.cern.ch/lib/lhcb/GAUSS/GAUSS_${GAUSS_VERSION}/Gen/LbPythia8/options/Pythia8.py \
 ${APPCONFIGOPTS}/Gauss/G4PL_FTFP_BERT_EmNoCuts.py \
-gaudi/Tags.py
+${MC_PROD}/gaudi/Tags.py
+
+cd ${MC_PROD}
