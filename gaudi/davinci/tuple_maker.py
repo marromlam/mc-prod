9a955e7d2d10a2f670d8b6bb859b6f1cb841dd7e
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2021-11-24 14:38:08 +0000

    change example tree to Bs0 one

--- gaudi/davinci/tuple_maker.py
+++ gaudi/davinci/tuple_maker.py
@@ -9,13 +9,14 @@ line = '$strippingline'
 
 dtt = DecayTreeTuple("DVTuple")
 dtt.Inputs = ['/Event/{0}/Phys/{1}/Particles'.format(stream, line)]
-# dtt.Decay = '[D*(2010)+ -> (D0 -> K- K+) pi+]CC'
-dtt.Decay = '[B0 -> (J/psi(1S) -> mu+ mu- {,gamma} {,gamma}) pi+ pi-]cc'
+# dtt.Decay = '[B_s0 -> mu+ mu- K+ K-]cc'
+dtt.Decay = 'B_s0 -> ^(J/psi(1S) -> ^mu+ ^mu-) ^(phi(1020) -> ^K+ ^K-)'
 
 # Configure DaVinci
 DaVinci().UserAlgorithms += [dtt]
 DaVinci().InputType = '$extension'.upper()
 DaVinci().TupleFile = '$output_file'
+DaVinci().HistogramFile = 'DV_histograms.root'
 DaVinci().PrintFreq = 1000
 DaVinci().DataType = '$year'
 DaVinci().Simulation = True
